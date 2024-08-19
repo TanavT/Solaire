a0fe42ae5242339e042b103b43db7b08000141b3
Tanav Thota <tanavthota@gmail.com>
2024-08-18 22:22:27 -0400

    Made and implemented base of Team Settings Framework as well as implement slash comman functionality

--- cogs/randomTeamGenerator.py
+++ cogs/randomTeamGenerator.py
@@ -1,6 +1,11 @@
 import discord
 from discord.ext import commands
+from ui.views.PlayerJoin import PlayerJoin
+from ui.views.randomTeamGeneratorH.TeamSettings import TeamSettings
 
+views = None
+players = None
+MAX_SLOTS = 20
 
 class RandomTeamMain(commands.Cog):
     def __init__(self, bot):
@@ -8,7 +13,32 @@ class RandomTeamMain(commands.Cog):
 
     @discord.slash_command()
     async def generate_random_team(self, ctx):
-        await ctx.respond(embed = discord.Embed())
+        global players
+        global MAX_SLOTS
+        global views
+
+        # variables
+        players = MAX_SLOTS * [""]
+
+        views = [PlayerJoin("| Waiting for Players...", players),
+                 TeamSettings("| Team Settings...", players),
+                 None]
+
+        await ctx.respond("| Starting Random Team Generator")
+
+        # running rush
+        iterator = 0
+        for view in views:
+            await ctx.send(content=view.message_str, view=view)
+            await view.wait()
+
+            if view.exit_triggered:
+                break
+            if iterator == 0:
+                while "" in players:
+                    players.remove("")
+            iterator += 1
+
 
 def setup(bot):
     bot.add_cog(RandomTeamMain(bot))
