bd4b7b663f7852054d4af35302ff4c94be213de7
Tanav Thota <tanavthota@gmail.com>
2024-09-23 20:02:34 -0400

    Areas -> Locations, works better with database

--- ui/views/raceH/gameViews/EldenRing/__init__.py
+++ ui/views/raceH/gameViews/EldenRing/__init__.py
@@ -29,7 +29,7 @@ class EldenRing(BaseView):
                 label="Boss",
             ),
             discord.SelectOption(
-                label="Area",
+                label="Locations",
             ),
             discord.SelectOption(
                 label="Runes",
